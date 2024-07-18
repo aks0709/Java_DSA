8c8ae84b5a2f9f8d2951d975a103e9a8a2e3691c
aks0709 <ayushkumarjgm@gmail.com>
2024-07-18 22:30:22 +0530

    REVISING TREE

---
+++ src/Tree/Revison/IsSameTree.java
@@ -0,0 +1,18 @@
+package Tree.Revison;
+
+public class IsSameTree {
+    //left se true return ho right se true return ho and jab dono  ki value same ho tab return ho
+
+//    public boolean isSameTree(TreeNode p, TreeNode q) {
+//        if(p==null||q==null)
+//            return p==q;
+//
+//        boolean leftTrue=isSameTree(p.left,q.left);
+//        boolean RightTrue=isSameTree(p.right,q.right);
+//
+//        if(leftTrue==true&&RightTrue==true&& (p.val==q.val))
+//            return true;
+//        else
+//            return false;
+//    }
+}
