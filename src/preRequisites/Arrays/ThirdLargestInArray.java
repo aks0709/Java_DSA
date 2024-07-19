64ba5ecd0922e975e37d53c870b8dccc436cdc28
aks0709 <ayushkumarjgm@gmail.com>
2024-07-19 07:21:27 +0530

    ARRAY QUESTION

---
+++ src/preRequisites/Arrays/ThirdLargestInArray.java
@@ -0,0 +1,28 @@
+package preRequisites.Arrays;
+
+public class ThirdLargestInArray {
+    //Given an integer array nums, return the third distinct maximum number in this array.
+    // If the third maximum does not exist, return the maximum number.
+    class Solution {
+        public int thirdMax(int[] nums) {
+            long max1=Long.MIN_VALUE;
+            long max2=Long.MIN_VALUE;
+            long max3=Long.MIN_VALUE;
+            for(int num : nums){
+                if(num>max1)
+                {
+                    max3=max2;
+                    max2=max1;
+                    max1=num;
+                }
+                else if(max1>num && num>max2){
+                    max3=max2;
+                    max2=num;
+                }else if(max2>num && num>max3){
+                    max3=num;
+                }
+            }
+            return max3!=Long.MIN_VALUE ? (int)max3 : (int)max1;
+        }
+    }
+}
