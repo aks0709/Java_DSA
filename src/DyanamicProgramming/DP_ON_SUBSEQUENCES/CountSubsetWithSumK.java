3de6d83f5ee7b4cd22ea9012efe4cd1a1c8fa97c
aks0709 <ayushkumarjgm@gmail.com>
2024-07-30 17:16:21 +0530

    DP on subsequences,handle case which have zero in input

--- src/DyanamicProgramming/DP_ON_SUBSEQUENCES/CountSubsetWithSumK.java
+++ src/DyanamicProgramming/DP_ON_SUBSEQUENCES/CountSubsetWithSumK.java
@@ -3,8 +3,6 @@ package DyanamicProgramming.DP_ON_SUBSEQUENCES;
 import java.util.Arrays;
 
 public class CountSubsetWithSumK {
-
-    //our approach
     class Solution {
         public static final int MOD = (int) 1e9 + 7;
 
@@ -18,9 +16,15 @@ public class CountSubsetWithSumK {
 
         public static int helper(int index, int[] arr, int target, int[][] dp) {
             // Base case
-            if (target == 0) return 1;
+            // if (target == 0) return 1;  this will cause problem
+            //suppose [0,0,1]  and our target is 1 and we start from back in that case
+            //{0,0,1}  {0,1}  {1}  these are possible subsets
+            //but according to base case we return as target==0
+
             if (index == 0) {
-                return arr[0] == target ? 1 : 0;
+
+                if(target==0 && arr[0]==0) return 2;
+                return arr[0] == target || target==0 ? 1 : 0;
             }
 
             // Memoization
@@ -41,40 +45,4 @@ public class CountSubsetWithSumK {
     }
 
 
-    //gfg approach
-    class Solution2 {
-        public static final int MOD = (int) 1e9 + 7;
-
-        public int perfectSum(int[] arr, int n, int sum) {
-            int[][] dp = new int[n + 1][sum + 1];
-            for (int[] row : dp) {
-                Arrays.fill(row, -1);
-            }
-
-            return fun(0,n, sum, arr,dp);
-        }
-
-        private int fun(int pos,int n, int sum, int[] arr,int dp[][]) {
-            if (pos >= n) {
-                return sum == 0 ? 1 : 0;
-            }
-
-            if (dp[pos][sum] != -1) {
-                return dp[pos][sum];
-            }
-
-
-
-            //not pick
-            int notpick = fun(pos + 1,n, sum, arr,dp)%MOD;
-
-            int pick=0;
-            //pick
-            if (arr[pos] <= sum) {
-                pick= fun(pos + 1, n,sum - arr[pos], arr,dp)%MOD;
-            }
-
-            return dp[pos][sum]=(pick+notpick)%MOD;
-        }
-    }
 }
