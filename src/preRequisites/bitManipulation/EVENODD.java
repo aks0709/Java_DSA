2dc3db8fca15d74c99394677308b03c8be8ca17a
aks0709 <ayushkumarjgm@gmail.com>
2024-08-31 00:35:44 +0530

    EVEN OR ODD

---
+++ src/preRequisites/bitManipulation/EVENODD.java
@@ -0,0 +1,9 @@
+package preRequisites.bitManipulation;
+
+public class EVENODD {
+    static String oddEven(int n) {
+        // code here
+        if((n&1)==1) return "odd";
+        else return "even";
+    }
+}
