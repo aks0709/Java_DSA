091de69c52e61564af474e93b8c1022d9ff0dd61
aks0709 <ayushkumarjgm@gmail.com>
2024-09-02 01:51:11 +0530

    Accenture practise

--- src/Company/Accenture/KadanesAlgo.java
+++ src/Company/Accenture/KadanesAlgo.java
@@ -6,7 +6,7 @@ public class KadanesAlgo {
     public static void main(String[] args) {
         int arr[]={-2,1,-3,4,-1,2,1,-5,4};
         System.out.println(Arrays.toString(maxSubArray2(arr)));
-        System.out.println(maxSubArray(arr));
+        System.out.println(maxSubArray(arr));//
     }
     public static int[] maxSubArray2(int[] nums) {
         int maxSum=Integer.MIN_VALUE;
