6f19d3526c16f6502c6c354783572d3186749ce8
aks0709 <ayushkumarjgm@gmail.com>
2024-10-28 19:35:55 +0530

    Bit Manipulation

--- src/preRequisites/bitManipulation/Test.java
+++ src/preRequisites/bitManipulation/Test.java
@@ -2,6 +2,6 @@ package preRequisites.bitManipulation;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("HELLO");
+        System.out.println("Starting Bit manipulation again");
     }
 }
