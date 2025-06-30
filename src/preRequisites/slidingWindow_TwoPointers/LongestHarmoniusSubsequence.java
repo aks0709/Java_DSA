23bec929b4b4d28fa9bcb51c4b43afc97212eb4c
aks0709 <ayushkumarjgm@gmail.com>
2025-06-30 22:06:09 +0530

    Sliding Window practise

---
+++ src/preRequisites/slidingWindow_TwoPointers/LongestHarmoniusSubsequence.java
@@ -0,0 +1,21 @@
+package preRequisites.slidingWindow_TwoPointers;
+import java.util.*;
+public class LongestHarmoniusSubsequence {
+    class Solution {
+        public int findLHS(int[] nums) {
+            Arrays.sort(nums);
+            int l=0;
+            int r=0;
+            int maxLen=0;
+            while(r<nums.length){
+                while(nums[r]-nums[l]>1) l++;
+
+                if(nums[r]-nums[l]==1)
+                    maxLen=Math.max(maxLen,r-l+1);
+
+                r++;
+            }
+            return maxLen;
+        }
+    }
+}
